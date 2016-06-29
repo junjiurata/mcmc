081890b9612295992db82baa03807f8f741c3ce2
junjiurata <urata@person.kobe-u.ac.jp>
2016-06-29 12:14:20 +0900

    test

---
+++ od-mcmc/build/Debug/Cygwin_2-Windows/main-mc-od.o.d
@@ -0,0 +1,3 @@
+build/Debug/Cygwin_2-Windows/main-mc-od.o: main-mc-od.cpp MT.h
+
+MT.h:
---
+++ od-mcmc/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_2-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_2-Windows
+CND_ARTIFACT_NAME_Debug=od-mcmc
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_2-Windows/od-mcmc
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_2-Windows/package
+CND_PACKAGE_NAME_Debug=od-mcmc.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_2-Windows/package/od-mcmc.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_2-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_2-Windows
+CND_ARTIFACT_NAME_Release=od-mcmc
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_2-Windows/od-mcmc
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_2-Windows/package
+CND_PACKAGE_NAME_Release=od-mcmc.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_2-Windows/package/od-mcmc.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
